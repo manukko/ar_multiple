db9edddaf8d6130035fec9a32b3b4d5cca1b5378
manukko <45536493+manukko@users.noreply.github.com>
2019-01-11 00:43:26 +0100

    Update changeimage.js

--- changeimage.js
+++ changeimage.js
@@ -1,5 +1,14 @@
+var electric = document.querySelector('#electriccar').style.visibility = "hidden";
+var oil = document.getElementById("#oilcar").style.visibility = "visible";
 document.addEventListener("click", function(){
   console.log("ciao");
-  var image = document.querySelector('#image');
-  image.setAttribute('src', 'electriccar');
+  if(oil=="visible"){
+    oil="hidden";
+    electric="visible";
+  }
+  else{
+    oil="visible";
+    electric="hidden";
+  }
+  
 });
