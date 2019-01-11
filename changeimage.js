7d9f6e3055429b2b2aaf8702a5765218dec1f743
manukko <45536493+manukko@users.noreply.github.com>
2019-01-11 01:31:23 +0100

    Update changeimage.js

--- changeimage.js
+++ changeimage.js
@@ -1,16 +1,18 @@
 var electric = document.getElementById("electriccar1"); 
-electric.style.display = "none";
+electric.style.visibility = "hidden";
 var oil = document.getElementById("oilcar1");
-oil.style.display = "block";
+oil.style.visibility = "visible";
 document.addEventListener("click", function(){
   console.log("ciao");
-  if(oil.style.display=="block"){
-    oil.style.display="none";
-    electric.style.display="block";
+  
+  if(oil.style.visibility=="visible"){
+    oil.style.visibility="hidden";
+    electric.style.visibility="visible";
   }
+  
   else{
-    oil.style.display="block";
-    electric.style.display="none";
+    oil.style.visibility="visible";
+    electric.style.visibility="hidden";
   }
   
 });
