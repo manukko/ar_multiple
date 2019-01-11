597541c244d65f789a35761bf0c732b015da702f
manukko <45536493+manukko@users.noreply.github.com>
2019-01-11 16:05:20 +0100

    Update button.js

--- button.js
+++ button.js
@@ -1,4 +1,4 @@
-var btn = document.createElement("BUTTON");        // Create a <button> element
-var t = document.createTextNode("CLICK ME");       // Create a text node
-btn.appendChild(t);                                // Append the text to <button>
-document.body.appendChild(btn);  
+var button = document.getElementById("button");
+button.addEventListener("click", function(){
+  button.value = "See the stars!";
+});
