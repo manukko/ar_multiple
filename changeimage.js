6e6b281e7bac07fb444d1ed84c2cda58495c6fae
manukko <45536493+manukko@users.noreply.github.com>
2019-01-12 15:49:48 +0100

    Update changeimage.js

--- changeimage.js
+++ changeimage.js
@@ -1,22 +1,40 @@
 var i=0;
 var electric = document.getElementById("electriccar1"); 
-electric.setAttribute('visible', false);
 var oil = document.getElementById("oilcar1");
-oil.setAttribute('visible', true);
+var co2 = document.getElementById("co21"); 
+var fuelreserves = document.getElementById("fuelreserves1");
 
 document.addEventListener("click", function(){
   console.log("ciao");
   i=(i+1)%4;
   
   
-  if(oil.getAttribute('visible') === true){
+  if(i===1){
+    fuelreserves.setAttribute('visible', true);
+    co2.setAttribute('visible', false);
+    oil.setAttribute('visible', false);
+    electric.setAttribute('visible', false);
+  }
+  
+  if(i===2){
+    fuelreserves.setAttribute('visible', false);
+    co2.setAttribute('visible', true);
+    oil.setAttribute('visible', false);
+    electric.setAttribute('visible', false);
+  }
+  if(i===3){
+    fuelreserves.setAttribute('visible', false);
+    co2.setAttribute('visible', false);
+    oil.setAttribute('visible', true);
+    electric.setAttribute('visible', false);
+  }
+  
+  if(i===0){
+    fuelreserves.setAttribute('visible', false);
+    co2.setAttribute('visible', false);
     oil.setAttribute('visible', false);
     electric.setAttribute('visible', true);
   }
   
-  else{
-    oil.setAttribute('visible', true);
-    electric.setAttribute('visible', false);
-  }
   
 });
