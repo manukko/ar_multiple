067581f907510f08c302e5f78890af8cdaa45b8f
manukko <45536493+manukko@users.noreply.github.com>
2019-01-12 16:25:23 +0100

    Update disappear.js

--- disappear.js
+++ disappear.js
@@ -1,3 +1,3 @@
-setTimeout(function(){ alert("WELCOME TO MY UNIVERSE, TO EXPLORE ME PLEASE FOLLOW THE ROUTE!\
-1)Scan the marker in the wall to see my appeareance.\
-2)click on the screen to know new things about me!"); }, 1000);
+setTimeout(function(){ alert("WELCOME TO MY UNIVERSE, TO EXPLORE ME PLEASE FOLLOW THE ROUTE:\
+1)Scan the marker in the wall to see my appeareance\
+2)Click on the screen to know new things about me!"); }, 1000);
